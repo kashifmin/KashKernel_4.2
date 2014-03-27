79c634dc8d6d72e5486da0cc68d9ed5554b9f6f0
kashifmin <dailypctips@gmail.com>
2014-03-27 07:31:11 -0400

    Add intellidemand gov

--- kernel/include/linux/cpufreq.h
+++ kernel/include/linux/cpufreq.h
@@ -385,6 +385,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
 #endif
 
 
