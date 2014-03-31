be2498e366020640bf076c0802c56a609d9f8c9a
kashifmin <dailypctips@gmail.com>
2014-03-31 11:20:07 -0400

    af_unix: speedup /proc/net/unix

--- kernel/mtkbuild.sh
+++ kernel/mtkbuild.sh
@@ -6,7 +6,7 @@ sudo TARGET_PRODUCT=wiko TARGET_BUILD_VARIANT=user MTK_ROOT_CUSTOM=../mediatek/c
 ../mediatek/build/tools/mkimage arch/arm/boot/zImage KERNEL > out/wiko/zImage_p
 
 #Modules part
-make INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=./out/wiko/system INSTALL_MOD_DIR=./out/wiko/system android_modules_install
+make TARGET_BUILD_VARIANT=user INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=./out/wiko/system INSTALL_MOD_DIR=./out/wiko/system android_modules_install
 
 #Repack part - You need to set PATH var correctly poiting to a directory with https://github.com/bgcngm/mtk-tools.git and chmod +x mkbootimg
 #You need ramdisk directory too
